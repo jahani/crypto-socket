1a88df28b6464215dfd9b1ce59b22e8cc83af08a
Peyman Jahani <jahani.peyman@gmail.com>
2021-12-16 12:43:50 +0000

    Update to Ramzinex's new api

--- src/models/exchanges/RamzinexExchange.js
+++ src/models/exchanges/RamzinexExchange.js
@@ -2,9 +2,11 @@ const BaseExchange = require('../BaseExchange.js')
 
 const axios = require('axios');
 
+const url = 'https://ramzinex.com/exchange/api/v1.0/exchange/pairs/2';
+
 class RamzinexExchange extends BaseExchange {
 
-    // https://ramzinex.com/exchange/apikeys/guide
+    // https://ramzinex.com/exchange/apidocs/apidoc.html
 
     static Data() {
         return {
@@ -16,10 +18,14 @@ class RamzinexExchange extends BaseExchange {
 
     static FetchPrice() {
         
-        return axios.get('https://ramzinex.com/exchange/api/exchange/prices')
+        return axios.get(url)
         .then((res) => {
             if (res.status != 200) {
                 throw (`statusCode: ${res.status}`);
+            }
+
+            if (res.data.status != 0) {
+                throw('Ramizex response message status code is not zero');
             }
 
             return this.BestPrice(res.data);
@@ -28,8 +34,8 @@ class RamzinexExchange extends BaseExchange {
     }
 
     static BestPrice(data) {
-        let buy = parseInt(data.original.btcirr.sell/10);
-        let sell = parseInt(data.original.btcirr.buy/10);
+        let buy = parseInt(data.data.sell/10);
+        let sell = parseInt(data.data.buy/10);
         return {
             buy: buy,
             sell: sell
