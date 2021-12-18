1f49340e0554f689d952348217ae61daca1a4a26
Peyman Jahani <jahani.peyman@gmail.com>
2021-12-18 10:37:26 +0000

    Update Wallex model to use api

--- src/models/exchanges/WallexExchange.js
+++ src/models/exchanges/WallexExchange.js
@@ -1,11 +1,12 @@
 const BaseExchange = require('../BaseExchange.js')
 
 const axios = require('axios');
-const $ = require('cheerio');
-const url = 'https://wallex.ir/markets/btc-tmn';
+
+// https://api-docs.wallex.ir
+const url = 'https://wallex.ir/api/v2/markets';
 
 class WallexExchange extends BaseExchange {
-    
+
     static Data() {
         return {
             name: 'Wallex',
@@ -15,35 +16,30 @@ class WallexExchange extends BaseExchange {
     }
 
     static FetchPrice() {
-
-        return axios.get(url, { headers: {
-            'pragma': 'no-cache',
-            'dnt': '1',
-            'accept-encoding': 'gzip, deflate',
-            'accept-language': 'en,en-US;q=0.9,fa;q=0.8,tr;q=0.7',
-            'upgrade-insecure-requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'cache-control': 'no-cache',
-            'authority': 'wallex.ir',
-        }  })
+        
+        return axios.get(url)
         .then((res) => {
             if (res.status != 200) {
                 throw (`statusCode: ${res.status}`);
             }
-            let html = res.data;
-            let buy = $('#sellers-table > tbody > tr:nth-child(1) > td:nth-child(2)', html).text();
-            let sell = $('#buyers-table > tbody > tr:nth-child(1) > td:nth-child(2)', html).text();
 
-            buy = buy.trim().split(',').join('');
-            sell = sell.trim().split(',').join('');
+            if (res.data.success != true) {
+                throw('Wallex response message status code is not true');
+            }
 
-            return {
-                buy: parseInt(buy),
-                sell: parseInt(sell)
-            };
+            return this.BestPrice(res.data);
         });
 
+    }
+
+    static BestPrice(data) {
+        let priceData = data.result.symbols["BTC-TMN"].stats;
+        let buy = parseInt(priceData.askPrice);
+        let sell = parseInt(priceData.bidPrice);
+        return {
+            buy: buy,
+            sell: sell
+        };
     }
 
 }
